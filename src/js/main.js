51b15b6b63b827a6f4c74e5cf0bf93d23546740c
dylan <justbeawesome9@gmail.com>
2024-01-01 18:53:00 -0500

    main

--- src/js/main.js
+++ src/js/main.js
@@ -10,9 +10,10 @@ import quicksort from './algorithms/quicksort.js';
 import LSDradixsort from './algorithms/LSDradixsort.js';
 import countingsort from './algorithms/countingsort.js';
 import bucketsort from './algorithms/bucketsort.js';
+import combsort from './algorithms/combsort.js';
 
 const el = document.getElementById("container");
-const v = new Visualizer(bucketsort,10,100,el)
+const v = new Visualizer(combsort,10,100,el)
 
 document.addEventListener('click', () => {
     v.start()
