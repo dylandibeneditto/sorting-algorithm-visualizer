df0562c914faf4dc08be2444991282e3dc5f9349
dylan <justbeawesome9@gmail.com>
2024-01-01 19:14:33 -0500

    formatted cocktail sort

--- src/js/algorithms/cocktailsort.js
+++ src/js/algorithms/cocktailsort.js
@@ -6,13 +6,8 @@ export default function cocktailsort(list) {
 
     while (swapped == true) {
 
-        // reset the swapped flag on entering the
-        // loop, because it might be true from a
-        // previous iteration.
         swapped = false;
 
-        // loop from bottom to top same as
-        // the bubble sort
         for (let i = start; i < end - 1; ++i) {
             if (list[i] > list[i + 1]) {
                 let temp = list[i];
@@ -25,20 +20,13 @@ export default function cocktailsort(list) {
             }
         }
 
-        // if nothing moved, then listay is sorted.
         if (swapped == false)
             break;
 
-        // otherwise, reset the swapped flag so that it
-        // can be used in the next stage
         swapped = false;
 
-        // move the end point back by one, because
-        // item at the end is in its rightful spot
         end = end - 1;
 
-        // from top to bottom, doing the
-        // same comparison as in the previous stage
         for (let i = end - 1; i >= start; i--) {
             if (list[i] > list[i + 1]) {
                 let temp = list[i];
@@ -52,9 +40,6 @@ export default function cocktailsort(list) {
             }
         }
 
-        // increase the starting point, because
-        // the last stage would have moved the next
-        // smallest number to its rightful spot.
         start = start + 1;
     }
     return result
